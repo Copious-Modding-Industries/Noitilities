1de3cf3030f820ec3c8970ba0c6ed3a92999374c
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2022-11-07 15:04:24 +1100

    add init function

--- CL_init.lua
+++ CL_init.lua
@@ -1,1 +1,19 @@
 -- Initialization file, this will initialize the Copi Library.
+return {
+    --- @param path string
+    --- @return nil
+    init = function(path)
+        path = path:gsub("/$", "") .. "/"
+        local files = {
+            "ModuleLoader.lua",
+            "Modules/ECS.lua",
+            "Modules/PolyUtils.lua",
+            "Modules/Vec2.lua"
+        }
+        for i, v in ipairs(files) do
+            local m = ModTextFileGetContent(path .. v)
+            m = m:gsub("CL_PATH", path)
+            ModTextFileSetContent(path .. v, m)
+        end
+    end
+}
