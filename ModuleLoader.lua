a6fbce0cf194f54686f0f4ecc3495061f32dac87
Human <54731850+Ramiels@users.noreply.github.com>
2022-11-06 21:13:37 -0500

    Update ModuleLoader.lua

--- ModuleLoader.lua
+++ ModuleLoader.lua
@@ -1,1 +1,10 @@
 -- Module Loader file, this will dofile_once the requested modules.
+
+--- @param modules table A table of string module names, will call `dofile_once()` on each requested module.
+--- ***
+--- -@return -- Maybe return a table of return values? 
+function DofileModules( modules )
+    for _, module_name in ipairs(modules) do
+        dofile_once("#COPI_LIB_PATH#/" .. module_name)
+    end
+end
