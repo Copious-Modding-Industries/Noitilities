c611d4716c3622aaea52491608dda851493cdc7f
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2022-11-09 09:07:05 +1100

    add typings

--- Modules/Vec2.lua
+++ Modules/Vec2.lua
@@ -4,8 +4,15 @@
 --- @field One Vec2
 --- @field UnitX Vec2
 --- @field UnitY Vec2
+--- @operator call: Vec2
+--- @operator unm: Vec2
+--- @operator add: Vec2
+--- @operator sub: Vec2
+--- @operator mul: Vec2
+--- @operator div: Vec2
 local Vec2 = {}
 
+--- @return Vec2
 function Vec2:New(x, y)
     x = x or 0
     y = y or 0
@@ -13,10 +20,22 @@ function Vec2:New(x, y)
         x = x,
         y = y,
     }
+    setmetatable(o, Vec2)
     o.__index = Vec2
     return o;
 end
 
+--- @return Vec2
+function Vec2:NewFromRad(rad)
+    return Vec2:New(math.cos(rad), math.sin(rad))
+end
+
+--- @return Vec2
+function Vec2:NewFromDeg(deg)
+    return Vec2:NewFromRad(math.rad(deg))
+end
+
+--- @return Vec2
 function Vec2.Add(v1, v2)
     if type(v1) == "number" then
         v1 = {x = v1, y = v1}
@@ -27,6 +46,7 @@ function Vec2.Add(v1, v2)
     return Vec2:New(v1.x + v2.x, v1.y + v2.y)
 end
 
+--- @return Vec2
 function Vec2.Sub(v1, v2)
     if type(v1) == "number" then
         v1 = {x = v1, y = v1}
@@ -37,6 +57,7 @@ function Vec2.Sub(v1, v2)
     return Vec2:New(v1.x - v2.x, v1.y - v2.y)
 end
 
+--- @return Vec2
 function Vec2.Mul(v1, v2)
     if type(v1) == "number" then
         v1 = {x = v1, y = v1}
@@ -47,6 +68,7 @@ function Vec2.Mul(v1, v2)
     return Vec2:New(v1.x*v2.x, v1.y*v2.y)
 end
 
+--- @return Vec2
 function Vec2.Div(v1, v2)
     if type(v1) == "number" then
         v1 = {x = v1, y = v1}
@@ -57,6 +79,7 @@ function Vec2.Div(v1, v2)
     return Vec2:New(v1.x/v2.x, v1.y/v2.y)
 end
 
+--- @return number
 function Vec2.DotProduct(a, b)
     local ax, ay = a.x-b.x, a.y-b.y
     local bx, by = b.x-a.x, b.y-a.y
@@ -64,36 +87,47 @@ function Vec2.DotProduct(a, b)
     return dot
 end
 
+--- @return Vec2
 function Vec2.Abs(v)
     return Vec2:New(math.abs(v.x), math.abs(v.y))
 end
 
+--- @return number
 function Vec2.Magnitude(v)
     return math.sqrt(v.x^2 + v.y^2)
 end
 
+--- @return number
 function Vec2.MagnitudeSquared(v)
     return v.x^2 + v.y^2
 end
 
-
+--- @return Vec2
 function Vec2.Normalise(v)
     return v:Div(v:Magnitude())
 end
 
+--- @return boolean
 function Vec2.__eq(a, b)
     if a.x ~= b.x or a.y ~= b.y then return false end
     return true
 end
 
+--- @return Vec2
 function Vec2:__unm()
     self.x = -self.x
     self.y = -self.y
     return self
 end
 
+--- @return string
 function Vec2:__tostring()
-    return ("(%s, %s)"):format(self.x, self.y)
+    return ("(%s, %s)"):format(tostring(self.x), tostring(self.y))
+end
+
+--- @return number, number
+function Vec2.Unpack(v)
+    return v.x, v.y
 end
 
 setmetatable(Vec2, {
@@ -104,7 +138,7 @@ setmetatable(Vec2, {
         elseif k == "UnitY" then return Vec2:New(0, 1)
         end
     end,
-    __call = function(x, y)
+    __call = function(_, x, y)
         return Vec2:New(x, y)
     end,
     __add = Vec2.Add,
