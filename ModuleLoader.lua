3a3a5dbdc16ff6d3ee3defbb1b85a664a0ae4c92
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2022-11-22 22:30:40 +1100

    add Load function

--- ModuleLoader.lua
+++ ModuleLoader.lua
@@ -1,6 +1,15 @@
 -- Module Loader file, this will dofile_once the requested modules.
 NL_ModuleCache = {}
-return setmetatable({}, {
+return setmetatable({
+    Load = function (modules)
+        local loaded = {}
+        for _, v in ipairs(modules) do
+            NL_ModuleCache[v] = NL_ModuleCache[v] or dofile_once("NL_PATH/Modules/" .. v .. ".lua")
+            table.insert(loaded, NL_ModuleCache[v])
+        end
+        return table.unpack(loaded)
+    end
+}, {
     __index = function (_, k)
         NL_ModuleCache[k] = NL_ModuleCache[k] or dofile_once("NL_PATH/Modules/" .. k .. ".lua")
         return NL_ModuleCache[k]
