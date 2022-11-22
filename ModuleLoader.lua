d311fd106b368302b36f1663f9540634dd9f91a0
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2022-11-22 22:21:31 +1100

    change moduleloader file

--- ModuleLoader.lua
+++ ModuleLoader.lua
@@ -1,19 +1,8 @@
 -- Module Loader file, this will dofile_once the requested modules.
 NL_ModuleCache = {}
---- @alias Modules "Vec2"|"ECS"|"PolyUtils"|"Base64"|"DamageModel"|"Translations"
---- @param module Modules
---- @return any data Return type is specified by overloads
---- @overload fun(module: "Vec2"): Vec2
---- @overload fun(module: "ECS"): ECS
---- @overload fun(module: "PolyUtils"): PolyUtils
---- @overload fun(module: "Base64"): Base64
-local function getModule(module)
-    local data = NL_ModuleCache[module]
-    if data == nil then
-        data = dofile_once("NL_PATH/Modules/" .. module .. ".lua")
-        NL_ModuleCache[module] = data
+return setmetatable({}, {
+    __index = function (_, k)
+        NL_ModuleCache[k] = NL_ModuleCache[k] or dofile_once("NL_PATH/Modules/" .. k .. ".lua")
+        return NL_ModuleCache[k]
     end
-    return data
-end
-
-return getModule
+})
