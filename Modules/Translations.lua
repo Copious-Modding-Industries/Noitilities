033817bb80eaab0429cbd3f7707ae66825caa492
Gustavo <77560533+ofoxsmith@users.noreply.github.com>
2022-12-14 00:22:40 +1100

    add addkey, editkey, and removekey functions

--- Modules/Translations.lua
+++ Modules/Translations.lua
@@ -1,5 +1,6 @@
 ---@alias langs "en"|"ru"|"pt-br"|"es-es"|"de"|"fr-fr"|"it"|"pl"|"zh-cn"|"jp"|"ko"
 ---@alias translationTable table<langs, table<string, string>>
+local Translations = {}
 --[[
 --- @param key string The translation key you wish to write to"]
 --- @param value string The value you will assign to the key
@@ -30,7 +31,7 @@ local translations = {
 ```
 ]]
 --- ***
-function CSVGenFromTable(translation_table)
+function Translations.CSVGenFromTable(translation_table)
     -- Assume the table is indexed by language
     if translation_table["en"] ~= nil then
         local sorted = {}
@@ -43,9 +44,9 @@ function CSVGenFromTable(translation_table)
         translation_table = sorted
     end
     local csv = ""
-    local languages = {"en","ru","pt-br","es-es","de","fr-fr","it","pl","zh-cn","jp","ko"}
+    local languages = { "en", "ru", "pt-br", "es-es", "de", "fr-fr", "it", "pl", "zh-cn", "jp", "ko" }
     for key, translations in pairs(translation_table) do
-        setmetatable(translations, {__index = function(_, _) return "" end})
+        setmetatable(translations, { __index = function(_, _) return "" end })
         csv = csv .. key .. ","
         for language in pairs(languages) do
             csv = csv .. translations[language]
@@ -55,17 +56,69 @@ function CSVGenFromTable(translation_table)
     return csv
 end
 
-function AppendTranslationFile(fileOrTable)
+function Translations.AppendTranslationFile(fileOrTable)
     if not ModTextFileGetContent then
         error("AppendTranslationFile can only be called when ModTextFileGet/SetContent is available", 2)
     end
     if type(fileOrTable) == "string" then
         local csv = ModTextFileGetContent(fileOrTable)
-        ModTextFileSetContent("data/translations/common.csv", ModTextFileGetContent("data/translations/common.csv") .. "\n" .. csv)
-    else 
-        local csv = CSVGenFromTable(fileOrTable)
-        ModTextFileSetContent("data/translations/common.csv", ModTextFileGetContent("data/translations/common.csv") .. "\n" .. csv)
+        ModTextFileSetContent("data/translations/common.csv",
+            ModTextFileGetContent("data/translations/common.csv") .. "\n" .. csv)
+    else
+        local csv = Translations.CSVGenFromTable(fileOrTable)
+        ModTextFileSetContent("data/translations/common.csv",
+            ModTextFileGetContent("data/translations/common.csv") .. "\n" .. csv)
     end
+end
+
+local function strsplit(delimiter, text)
+    ---@diagnostic disable-next-line: unbalanced-assignments
+    local list, pos, first, last = {}, 1
+    while true do
+        first, last = text:find(delimiter, pos, true)
+        if first then -- found?
+            table.insert(list, text:sub(pos, first - 1))
+            pos = last + 1
+        else
+            table.insert(list, text:sub(pos))
+            break
+        end
+    end
+    return list
+end
+
+function Translations.AddKey(name, translations)
+    local csv = name .. ","
+    local languages = { "en", "ru", "pt-br", "es-es", "de", "fr-fr", "it", "pl", "zh-cn", "jp", "ko" }
+    for language in pairs(languages) do
+        csv = csv .. (translations[language] or "") .. ","
+    end
+    csv = csv .. "\n"
+    ModTextFileSetContent("data/translations/common.csv",
+        ModTextFileGetContent("data/translations/common.csv") .. "\n" .. csv)
+end
+
+function Translations.EditKey(name, translations)
+    local csv = ModTextFileGetContent("data/translations/common.csv")
+    local defaults = strsplit(",", csv:match(name .. ",([^\n]+)\n"))
+    local languages = { "en", "ru", "pt-br", "es-es", "de", "fr-fr", "it", "pl", "zh-cn", "jp", "ko" }
+    for i=1, #defaults do local v = defaults[i]
+        defaults[languages[i]] = v
+    end
+    csv = csv:gsub(name .. "[^\n]+\n", "")
+    local newCsv = name .. ","
+    for language in pairs(languages) do
+        newCsv = newCsv .. (translations[language] or defaults[language]) .. ","
+    end
+    newCsv = newCsv .. "\n"
+    csv = csv .. "\n" .. newCsv
+    ModTextFileSetContent("data/translations/common.csv", csv)
+end
+
+function Translations.RemoveKey(name)
+    local csv = ModTextFileGetContent("data/translations/common.csv")
+    csv = csv:gsub(name .. "[^\n]+\n", "")
+    ModTextFileSetContent("data/translations/common.csv", csv)
 end
 
 local keytrans = {
@@ -90,3 +143,5 @@ local keytrans = {
         ["pt-br"] = "Um projétil fraco, reluzente e encantador.",
     },
 }
+
+return Translations
